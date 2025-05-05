55920601fd9bb74e64c5587ce434c8130aa2d92a
Shubham Ranga <shubhamranga@Shubhams-MacBook-Air.local>
2025-05-05 11:49:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Dice',
+        style: TextStyle(
+          color: Colors.white
+        ),
+        ),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
